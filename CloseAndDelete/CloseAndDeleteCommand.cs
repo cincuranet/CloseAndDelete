bf3c76e02b7b5e0a2cbbf49d8ada63af7682439c
Jiri Cincura <jiri@x2develop.com>
2017-03-20 08:24:38 +0100

    Initial sources.

---
+++ CloseAndDelete/CloseAndDeleteCommand.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio;
+using System.IO;
+
+namespace CloseAndDelete
+{
+    internal sealed class CloseAndDeleteCommand
+    {
+        public const int CommandId = 0x0100;
+        public static readonly Guid CommandSet = new Guid("acff2309-824d-4bb9-b3d9-766f164d13e0");
+
+        readonly Package _package;
+
+        CloseAndDeleteCommand(Package package)
+        {
+            _package = package ?? throw new ArgumentNullException(nameof(package));
+
+            if (ServiceProvider.GetService(typeof(IMenuCommandService)) is OleMenuCommandService commandService)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        public static CloseAndDeleteCommand Instance { get; private set; }
+
+        IServiceProvider ServiceProvider => _package;
+
+        public static void Initialize(Package package)
+        {
+            Instance = new CloseAndDeleteCommand(package);
+        }
+
+        void MenuItemCallback(object sender, EventArgs e)
+        {
+            if (ShowMessage("Are you sure?", OLEMSGICON.OLEMSGICON_QUERY, OLEMSGBUTTON.OLEMSGBUTTON_YESNO) != (int)VSConstants.MessageBoxResult.IDYES)
+            {
+                return;
+            }
+
+            if (ServiceProvider.GetService(typeof(IVsSolution)) is IVsSolution solution)
+            {
+                try
+                {
+                    if (solution.GetSolutionInfo(out var solutionDirectory, out _, out _) == VSConstants.S_OK && !string.IsNullOrEmpty(solutionDirectory))
+                    {
+                        if (solution.CloseSolutionElement((uint)__VSSLNSAVEOPTIONS.SLNSAVEOPT_NoSave, null, 0) == VSConstants.S_OK)
+                        {
+                            DeleteDirectoryRetry(solutionDirectory);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ShowMessage(ex.ToString(), OLEMSGICON.OLEMSGICON_CRITICAL, OLEMSGBUTTON.OLEMSGBUTTON_OK);
+                }
+            }
+        }
+
+        int ShowMessage(string message, OLEMSGICON icon, OLEMSGBUTTON buttons)
+        {
+            return VsShellUtilities.ShowMessageBox(
+                ServiceProvider,
+                message,
+                string.Empty,
+                icon,
+                buttons,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+
+        static void DeleteDirectoryRetry(string directory)
+        {
+            var cnt = 0;
+            while (true)
+            {
+                try
+                {
+                    cnt++;
+                    Directory.Delete(directory, true);
+                    break;
+                }
+                catch (IOException) when (cnt < 3)
+                { }
+            }
+        }
+    }
+}
---
+++ CloseAndDelete/CloseAndDeleteCommandPackage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
+
+namespace CloseAndDelete
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(CloseAndDeleteCommandPackage.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class CloseAndDeleteCommandPackage : Package
+    {
+        public const string PackageGuidString = "653ed5b1-beb3-447e-b3ba-72f5ccb222b9";
+
+        public CloseAndDeleteCommandPackage()
+        { }
+
+        protected override void Initialize()
+        {
+            CloseAndDeleteCommand.Initialize(this);
+            base.Initialize();
+        }
+    }
+}
